04ca265bcefd7e41a7a695f3ab9aea2bb42d9ae9
Unknown <xemnaskami@gmail.com>
2018-05-13 12:12:17 +0100

    Skills to skills.php

---
+++ skills.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antoinepyre
+ * Date: 13/05/2018
+ * Time: 11:56
+ */
+
+    function buildTableRow($imgSource, $title, $skillLevel) {
+        echo "
+        <tr>
+            <td class=\"two wide\">
+                <img class=\"ui tiny image\" src=\"$imgSource\"/>
+            </td>
+            <td class=\"two wide\">
+                <b>$title</b>
+            </td>
+            <td class=\"twelve wide\">
+                <div class=\"ui indicating progress my-bar\" data-value=\"$skillLevel\" data-total=\"5\">
+                    <div class=\"bar\"></div>
+    
+                </div>
+            </td>
+        </tr>
+ 
+        ";
+    }
+?>
+
+<table class="ui table">
+    <thead>
+    <tr>
+        <th colspan="3">
+            Dévelopement
+        </th>
+    </tr>
+    </thead>
+    <tbody>
+        <?php buildTableRow("images/cpp.png", "C/C++", "4")?>
+        <?php buildTableRow("images/py.png", "Python", "4")?>
+        <?php buildTableRow("images/webStack.png", "FrontEnd", "3")?>
+        <?php buildTableRow("images/react_logo.svg", "React Native", "4")?>
+    </tbody>
+</table>
+<table class="ui table">
+    <thead>
+    <tr>
+        <th colspan="3">
+            Langues
+        </th>
+    </tr>
+    </thead>
+    <tbody>
+        <?php buildTableRow("images/flag-round-fr.png", "Français", "5")?>
+        <?php buildTableRow("images/uk.jpg", "English", "4")?>
+    </tbody>
+</table>
+
